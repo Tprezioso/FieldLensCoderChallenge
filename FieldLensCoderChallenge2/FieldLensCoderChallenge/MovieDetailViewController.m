dc9fc8849b77e9721004af9b59bbd030bc495821
Tprezioso <tommyprezioso@gmail.com>
2015-06-08 11:46:04 -0400

    clean up and refactoring

--- FieldLensCoderChallenge2/FieldLensCoderChallenge/MovieDetailViewController.m
+++ FieldLensCoderChallenge2/FieldLensCoderChallenge/MovieDetailViewController.m
@@ -29,9 +29,6 @@
     
     self.movieTitle.text = self.titleOfMovie;
    
-   // UIImage *damnImage = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://farm4.static.flickr.com/3092/2915896504_a88b69c9de.jpg"]]];
-   
-    
     self.movieImage.image = self.moviePosterImage;
     
     self.movieDescription.text = self.overview;
@@ -42,15 +39,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
