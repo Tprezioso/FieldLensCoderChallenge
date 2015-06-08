92424fac2873124065f73254c97169e57a56870b
Tprezioso <tommyprezioso@gmail.com>
2015-06-08 11:48:41 -0400

    refactoring some more

--- FieldLensCoderChallenge2/FieldLensCoderChallenge/APIClient.h
+++ FieldLensCoderChallenge2/FieldLensCoderChallenge/APIClient.h
@@ -2,7 +2,7 @@
 //  APIClient.h
 //  FieldLensCoderChallenge
 //
-//  Created by Anish Kumar on 6/6/15.
+//  Created by Thomas Prezioso on 6/6/15.
 //  Copyright (c) 2015 Thomas Prezioso. All rights reserved.
 //
 
